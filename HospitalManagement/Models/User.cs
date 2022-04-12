13fd091de12707cbdc7f2f81b5f86a259d20aded
Nikola <nikoladevelops@gmail.com>
2022-04-12 19:15:09 +0300

    Created models for a User,Role,MedicalCondition

---
+++ HospitalManagement/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace HospitalManagement.Models
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
