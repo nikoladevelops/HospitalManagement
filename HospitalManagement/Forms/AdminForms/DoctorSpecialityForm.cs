641e7a4b1c5b4685e383bbc29e1f6bb02a29330c
Nikola <nikoladevelops@gmail.com>
2022-04-16 13:13:05 +0300

    Fixed a bug. The doctors were being deleted automatically when deleting a DoctorSpeciality, but their User accounts were left, which broke out search doctor logic.

--- HospitalManagement/Forms/AdminForms/DoctorSpecialityForm.cs
+++ HospitalManagement/Forms/AdminForms/DoctorSpecialityForm.cs
@@ -1,4 +1,5 @@
 ﻿using HospitalManagement.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -76,6 +77,33 @@ namespace HospitalManagement.Forms.AdminForms
                 if (result == DialogResult.Yes)
                 {
                     var selectedSpeciality = db.DoctorSpecialities.Single(x => x.Name == specialityListBox.SelectedItem.ToString());
+                    
+                    // намери ми всички доктори с избраната специалност
+                    var allDoctorsWithSpeciality = db.Doctors
+                        .Include(d => d.DoctorSpeciality)
+                        .Where(d => d.DoctorSpeciality.Name == selectedSpeciality.Name)
+                        .ToList();
+
+                    // изтрии всички User акаунти към които сочат докторите
+                    // заедно с тях ще се изтрие и информацията на докторите
+                    // автоматично тъй като таблиците са ни настроени
+                    // с CASCADE опция за изтриване
+                    // (ако изтрием ред на таблица,а той бива използван като foreign key
+                    // в друга таблица, то тогава там където се ползва ще бъде изтрита информацията също и накрая той )
+                    foreach (var doctor in allDoctorsWithSpeciality)
+                    {
+                        var doctorUserAccount = db.Users.Single(u => u.Id == doctor.UserId);
+                        db.Users.Remove(doctorUserAccount);
+                    }
+
+                    // ако нямахме горния код отгоре, а само тука този отдолу
+                    // по CASCADE начин щяха да бъдат изтрити всички доктори с тази специалност,
+                    // но проблема е че USER акаунтите на докторите щяха да останат в базата
+                    // което би довело до бъгове, тъй като USERITE макар да нямат доктор на който
+                    // да съответства (тъй като той бива изтрит) , ролята все още им е Doctor
+                    // а ние НЕ искаме това, именно затова трием самите User акаунти 
+                    // -> от там автоматично ни се трият докторите, които сочат с foreign key към тях
+                    // и сега вече като сме се отървали от докторите, можем да изтрием и самата докторска специалност
                     db.DoctorSpecialities.Remove(selectedSpeciality);
                     await db.SaveChangesAsync();
                     LoadSpecialityListBoxData();
