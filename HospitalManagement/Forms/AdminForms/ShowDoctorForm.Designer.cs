0390b5405bdced533c377e8b7080557874697f69
Nikola <nikoladevelops@gmail.com>
2022-04-17 15:30:34 +0300

    Changed the design of ShowDoctorForm

--- HospitalManagement/Forms/AdminForms/ShowDoctorForm.Designer.cs
+++ HospitalManagement/Forms/AdminForms/ShowDoctorForm.Designer.cs
@@ -28,33 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.specialityLabel = new System.Windows.Forms.Label();
+            this.prescriptionsAmountLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.middleNameLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // tableLayoutPanel1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(297, 85);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(136)))), ((int)(((byte)(255)))));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1.026958F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 98.97305F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 36;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.middleNameLabel);
+            this.panel1.Controls.Add(this.lastNameLabel);
+            this.panel1.Controls.Add(this.prescriptionsAmountLabel);
+            this.panel1.Controls.Add(this.specialityLabel);
+            this.panel1.Controls.Add(this.firstNameLabel);
+            this.panel1.Controls.Add(this.passwordLabel);
+            this.panel1.Controls.Add(this.emailLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(11, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(777, 444);
+            this.panel1.TabIndex = 0;
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.emailLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(246)))), ((int)(((byte)(250)))));
+            this.emailLabel.Location = new System.Drawing.Point(186, 57);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(79, 25);
+            this.emailLabel.TabIndex = 31;
+            this.emailLabel.Text = "Имейл: ";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.passwordLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(246)))), ((int)(((byte)(250)))));
+            this.passwordLabel.Location = new System.Drawing.Point(186, 82);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(87, 25);
+            this.passwordLabel.TabIndex = 32;
+            this.passwordLabel.Text = "Парола: ";
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.firstNameLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(246)))), ((int)(((byte)(250)))));
+            this.firstNameLabel.Location = new System.Drawing.Point(186, 134);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(58, 25);
+            this.firstNameLabel.TabIndex = 33;
+            this.firstNameLabel.Text = "Име: ";
+            // 
+            // specialityLabel
+            // 
+            this.specialityLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.specialityLabel.AutoSize = true;
+            this.specialityLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.specialityLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(246)))), ((int)(((byte)(250)))));
+            this.specialityLabel.Location = new System.Drawing.Point(186, 210);
+            this.specialityLabel.Name = "specialityLabel";
+            this.specialityLabel.Size = new System.Drawing.Size(135, 25);
+            this.specialityLabel.TabIndex = 34;
+            this.specialityLabel.Text = "Специалност: ";
+            // 
+            // prescriptionsAmountLabel
+            // 
+            this.prescriptionsAmountLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.prescriptionsAmountLabel.AutoSize = true;
+            this.prescriptionsAmountLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.prescriptionsAmountLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(246)))), ((int)(((byte)(250)))));
+            this.prescriptionsAmountLabel.Location = new System.Drawing.Point(186, 293);
+            this.prescriptionsAmountLabel.Name = "prescriptionsAmountLabel";
+            this.prescriptionsAmountLabel.Size = new System.Drawing.Size(228, 25);
+            this.prescriptionsAmountLabel.TabIndex = 35;
+            this.prescriptionsAmountLabel.Text = "Брой издадени рецепти: ";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lastNameLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(246)))), ((int)(((byte)(250)))));
+            this.lastNameLabel.Location = new System.Drawing.Point(186, 185);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(100, 25);
+            this.lastNameLabel.TabIndex = 36;
+            this.lastNameLabel.Text = "Фамилия: ";
+            // 
+            // middleNameLabel
+            // 
+            this.middleNameLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.middleNameLabel.AutoSize = true;
+            this.middleNameLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.middleNameLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(246)))), ((int)(((byte)(250)))));
+            this.middleNameLabel.Location = new System.Drawing.Point(186, 160);
+            this.middleNameLabel.Name = "middleNameLabel";
+            this.middleNameLabel.Size = new System.Drawing.Size(98, 25);
+            this.middleNameLabel.TabIndex = 37;
+            this.middleNameLabel.Text = "Презиме: ";
             // 
             // ShowDoctorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "ShowDoctorForm";
-            this.Text = "ShowDoctorForm";
+            this.Text = "Информация за доктора";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private Label label1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Panel panel1;
+        private Label middleNameLabel;
+        private Label lastNameLabel;
+        private Label prescriptionsAmountLabel;
+        private Label specialityLabel;
+        private Label firstNameLabel;
+        private Label passwordLabel;
+        private Label emailLabel;
     }
 }
--- HospitalManagement/Forms/AdminForms/ShowDoctorForm.cs
+++ HospitalManagement/Forms/AdminForms/ShowDoctorForm.cs
@@ -1,4 +1,5 @@
 ﻿using HospitalManagement.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -18,10 +19,32 @@ namespace HospitalManagement.Forms.AdminForms
         {
             InitializeComponent();
         }
+        // тази форма трябва да се ползва само със User-и, които са в роля Doctor
+        // ако сложим user с друга роля ще получим грешка, имай го на предвид
         public ShowDoctorForm(ApplicationDbContext db, User user):this()
         {
             this.db = db;
-            this.label1.Text = user.Email;
+            this.emailLabel.Text += user.Email;
+            this.passwordLabel.Text += user.Password;
+            // вземи информацията на доктора за съответния user
+            // знаем че ползваме .Single() понеже имаме връзка едно към едно
+            // точно това би хвърлило грешката ако user-а всъщност няма докторски акаунт
+            var doctorInfo = db.Doctors
+                .Include(d => d.DoctorSpeciality)
+                .Single(d => d.UserId == user.Id);
+
+            // вземи колко рецепти е изписал този доктор
+            var doctorPrescriptionCount = db.Prescriptions
+                .Where(p => p.DoctorId == doctorInfo.Id)
+                .Count();
+
+            this.firstNameLabel.Text += doctorInfo.FirstName;
+            this.middleNameLabel.Text += doctorInfo.MiddleName;
+            this.lastNameLabel.Text += doctorInfo.LastName;
+            this.specialityLabel.Text += doctorInfo.DoctorSpeciality.Name;
+            this.prescriptionsAmountLabel.Text += doctorPrescriptionCount;
+
+
         }
     }
 }
